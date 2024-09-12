406d997344fd2e221d7c0b90f51dfd89663c1080
mikhaelr12 <rusumiihai@gmail.com>
2024-09-12 13:53:40 +0300

    Initial commit

---
+++ src/main/java/md/orange/preorderback/PreOrderBackApplication.java
@@ -0,0 +1,13 @@
+package md.orange.preorderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOrderBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreOrderBackApplication.class, args);
+    }
+
+}
