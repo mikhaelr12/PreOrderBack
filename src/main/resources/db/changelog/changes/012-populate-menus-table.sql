3b648aab0da0ad66796ca88fabca44a415269a1e
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-09-18 14:27:10 +0300

    Added script to populate locations and menus tables

---
+++ src/main/resources/db/changelog/changes/012-populate-menus-table.sql
@@ -0,0 +1,41 @@
+INSERT INTO menus (id, location_id)
+VALUES
+    -- Meniuri pentru locația 1 (Andys - Str. Unirii 12)
+    (nextval('menu_id_seq'), 1),
+    (nextval('menu_id_seq'), 2),
+
+    -- Meniuri pentru locația 2 (Corso - Bd. Victoriei 45)
+    (nextval('menu_id_seq'), 3),
+    (nextval('menu_id_seq'), 4),
+
+    -- Meniuri pentru locația 3 (La Placinte - Str. Stefan cel Mare 34)
+    (nextval('menu_id_seq'), 5),
+    (nextval('menu_id_seq'), 6),
+
+    -- Meniuri pentru locația 4 (Foisor - Str. Eminescu 28)
+    (nextval('menu_id_seq'), 7),
+    (nextval('menu_id_seq'), 8),
+
+    -- Meniuri pentru locația 5 (Torro Burgers - Str. Republicii 100)
+    (nextval('menu_id_seq'), 9),
+    (nextval('menu_id_seq'), 10),
+
+    -- Meniuri pentru locația 6 (Pegas Burger - Bd. Independentei 75)
+    (nextval('menu_id_seq'), 11),
+    (nextval('menu_id_seq'), 12),
+
+    -- Meniuri pentru locația 7 (Esushi - Str. Libertatii 22)
+    (nextval('menu_id_seq'), 13),
+    (nextval('menu_id_seq'), 14),
+
+    -- Meniuri pentru locația 8 (Vasabi - Bd. Mihai Viteazul 67)
+    (nextval('menu_id_seq'), 15),
+    (nextval('menu_id_seq'), 16),
+
+    -- Meniuri pentru locația 9 (Starkebab - Str. Grivitei 19)
+    (nextval('menu_id_seq'), 17),
+    (nextval('menu_id_seq'), 18),
+
+    -- Meniuri pentru locația 10 (Twister - Bd. Dacia 88)
+    (nextval('menu_id_seq'), 19),
+    (nextval('menu_id_seq'), 20);
