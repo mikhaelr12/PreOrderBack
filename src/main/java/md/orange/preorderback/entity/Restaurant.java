6b971632bef4ea7960ae9695972ad1f445de8f33
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-22 18:09:40 +0200

    Added CRUD for Restaurant entity

--- src/main/java/md/orange/preorderback/entity/Restaurant.java
+++ src/main/java/md/orange/preorderback/entity/Restaurant.java
@@ -2,11 +2,13 @@ package md.orange.preorderback.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity(name = "restaurants")
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Restaurant {
