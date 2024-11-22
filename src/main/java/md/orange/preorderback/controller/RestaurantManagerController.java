380425c9ecd05a4617f452dee22f16e6962cb78c
Lungu Mihai <lungu2003mihai1@mail.ru>
2024-11-22 18:10:06 +0200

    Added new Controller for Restaurant CRUD

--- src/main/java/md/orange/preorderback/controller/RestaurantManagerController.java
+++ src/main/java/md/orange/preorderback/controller/RestaurantManagerController.java
@@ -1,4 +1,33 @@
 package md.orange.preorderback.controller;
 
+import lombok.AllArgsConstructor;
+import md.orange.preorderback.dto.RestaurantDTO;
+import md.orange.preorderback.service.RestaurantManagerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/restaurant-manager")
+@AllArgsConstructor
 public class RestaurantManagerController {
+
+    private final RestaurantManagerService restaurantManagerService;
+
+    @PostMapping
+    public ResponseEntity<?> addRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
+        restaurantManagerService.addRestaurant(restaurantDTO);
+        return ResponseEntity.ok("Restaurant added");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRestaurant(@PathVariable Long id) {
+        restaurantManagerService.deleteRestaurant(id);
+        return ResponseEntity.ok("Restaurant deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateRestaurant(@PathVariable Long id, @RequestBody RestaurantDTO restaurantDTO) {
+        restaurantManagerService.updateRestaurant(id, restaurantDTO);
+        return ResponseEntity.ok("Restaurant updated");
+    }
 }
